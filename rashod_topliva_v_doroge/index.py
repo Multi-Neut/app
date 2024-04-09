bdaed043a29175cb0447c33b93e3e9604f68a2a5
Multi-Neut <graf.alex001@gmail.com>
2024-04-09 22:00:36 +0800

    Vvel variables

---
+++ rashod_topliva_v_doroge/index.py
@@ -0,0 +1,7 @@
+flow_na_100_km = 0      # Расход на 100 км
+price_za_liter = 0      # Цена за литр
+total_price = 0     # Общая сумма денег
+gasTant_volume = 0      # Объем бензобака
+kalichestvo_zapravok = 0        # кол-во заправок
+all_km = 0      # всего км
+all_gasTank = 0     # всего бензина
